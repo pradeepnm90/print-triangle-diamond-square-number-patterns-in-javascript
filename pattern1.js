db3b77163536449eab66ddca84ff96d8ce3ec9a2
prad1490 <dheep2lrn@gmail.com>
2018-11-06 16:17:19 +0530

    star pattern logic

---
+++ pattern1.js
@@ -0,0 +1,15 @@
+function f(v){
+
+    for ( var i = 1; i<=v; i++){
+var chars="";
+	  for (var j= 1; j<=v;  j++){
+	
+	     if(j<= v+1-i){ chars += "*";} 
+		 
+	   }
+		console.log(chars);
+		 
+	}
+}
+
+f(7);
---
+++ pyramid.js
@@ -0,0 +1,20 @@
+//1, 3, 5,7,9..     ---> Odd number series (stars) appear in pyramid pattern
+//1, 2, 3,4,5..   ----> Counter (number of rows)
+
+//For every each counter there exist 2*n-1 value
+
+//1     *    
+//2    ***   
+//3   *****  
+//4  ******* 
+//5 *********
+function pyramid(n) {  //Input or number of rows
+    for (var i = 1; i <= n; i++) {
+        var s = "";
+        for (var j = 1; j <= (2 * n - 1); j++) {   ////For every each counter there exist 2*n-1 value
+            (j >= n + 1 - i && j <= n - 1 + i) ? s += "*" : s += " ";   //Check the work book image
+        }
+        console.log(s);
+    }
+}
+pyramid(5);
